2caa6f9b58f66eef59139d39e3c1278943490dec
David Yushkov <davidyushkov@gmail.com>
2022-03-06 15:43:24 +0200

    added some assets and tiles

--- src/Utils.gd
+++ src/Utils.gd
@@ -16,3 +16,6 @@ func get_random(min_num: int, max_num: int) -> int:
 	var rng = RandomNumberGenerator.new()
 	rng.randomize()
 	return rng.randi_range(min_num, max_num)
+
+func calc_damage(damage: float, armor: float, attack_type: String, armor_type: String) -> float:
+		return damage
