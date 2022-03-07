a3cfd49ee67ab8116ee5957d1691382501fa17b9
David Yushkov <davidyushkov@gmail.com>
2022-03-07 16:39:47 +0200

    return of the get_parent

--- src/Levels/LevelTemplate/LevelTemplate.gd
+++ src/Levels/LevelTemplate/LevelTemplate.gd
@@ -8,6 +8,7 @@ var level_completed := false
 
 func is_level_completed() -> bool:
 	#TODO need to fix, this function runs before current removing path
+	#we can make sum of all children if there is only one then level is completed
 	if spawns_melee.get_child_count() == 0 and spawns_range.get_child_count() == 0:
 		level_completed = true
 		print('Level completed')
